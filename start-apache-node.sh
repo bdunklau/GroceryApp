040d446d670aa9c388bbb2323bd04c2692d98328
EC2 Default User <ec2-user@ip-172-31-28-156.us-east-2.compute.internal>
2020-04-20 21:36:24 +0000

    run the Grocery client code on port 4201

--- start-apache-node.sh
+++ start-apache-node.sh
@@ -1,10 +1,6 @@
 # restart apache
 ~/GroceryApp/restartapache.sh
 
-# start the angular app
-cd ~/GroceryApp/client
-npm start &
-
 # start the node app
 cd ~/GroceryApp/server
 node app.js &
--- stop-apache-node.sh
+++ stop-apache-node.sh
@@ -1,9 +1,6 @@
 #stop apache
 sudo systemctl stop httpd
 
-#stop angular
-sudo kill $(sudo lsof -t -i:4201)
-
 #stop node
 sudo kill $(sudo lsof -t -i:3001)
 
